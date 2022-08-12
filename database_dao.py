b47857640bbc8ef7fd8dffeaee18193e85313825
mk_work <mknord1565@gmail.com>
2022-08-12 13:25:32 +0300

    improvements

--- database_dao.py
+++ database_dao.py
@@ -14,9 +14,7 @@ class DatabaseDAO:
         with sqlite3.connect(self.sourse) as connection:
             cursor = connection.cursor()
             cursor.execute(query)
-            result = []
-            for row in cursor:
-                result.append(row)
+            result = cursor.fetchall()
             return result
 
     def _get_formatted_result(self, *args, data_from_db) -> List[Dict]:
@@ -43,6 +41,7 @@ class DatabaseDAO:
         return result
 
     def search_by_years_range(self, year_1, year_2) -> List[Dict[AnyStr, AnyStr]]:
+        """Create query and get data from db by years rane"""
         query = f"""
                     select title, release_year
                     from netflix
@@ -65,6 +64,7 @@ class DatabaseDAO:
         return None
 
     def search_by_rating(self, rating_group: str) -> List[Dict[AnyStr, AnyStr]]:
+        """Create query and get data from db by rating"""
         rating_query = self._define_rating_query_part(rating_group)
         query = f"""
                     select title, rating, description
@@ -77,6 +77,7 @@ class DatabaseDAO:
         return result
 
     def search_by_genre(self, genre: str) -> List[Dict[AnyStr, AnyStr]]:
+        """Create query and get data from db by genre"""
         query = f"""
                     select title, description
                     from netflix
@@ -89,6 +90,7 @@ class DatabaseDAO:
         return result
 
     def search_by_type_release_year_genre(self, type, release_year, genre):
+        """Create query and get data from db by type, release year, genre"""
         query = f"""
                     select type, description
                     from netflix
@@ -101,5 +103,37 @@ class DatabaseDAO:
         result = self._get_formatted_result('title', 'description', data_from_db=data_from_db)
         return result
 
+    def search_actors_wierd_foo(self, actor_1, actor_2):
+        """
+        Функция, которая получает в качестве аргумента имена двух актеров,
+        сохраняет всех актеров из колонки cast и возвращает список тех, кто играет с ними в паре больше 2 раз
+        """
+
+        query = f"""
+                    select netflix.cast
+                    from netflix
+                    where netflix.cast like '%{actor_1}%' and netflix.cast like '%{actor_2}%'
+        """
+        data_from_db = self._get_data_from_db_by_query(query)
+
+        # Находим всех актеров, которые одновременно играли в паре с actor_1 и actor_2
+        for group in data_from_db:
+            all_persons = set()
+            for person in group[0].split(', '):
+                if person not in (actor_1, actor_2):
+                    all_persons.add(person)
+
+        # Находим актеров, которые одновременно играли в паре с actor_1 и actor_2 более 2х раз
+        finded_person = []
+        for person in all_persons:
+            counter = 0
+            for group in data_from_db:
+                if person in group[0].split(', '):
+                    counter += 1
+            if counter > 2:
+                finded_person.append(person)
+
+        return finded_person
+
 # db_dao = DatabaseDAO()
-# print(db_dao.search_by_type_release_year_genre('tv', 2010, 'comed'))
+# print(db_dao.search_actors_wierd_foo('Jack Black', 'Dustin Hoffman'))

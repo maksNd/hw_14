d5ea910a4aafba86998a21d975a4766ad3efeb43
mk_work <mknord1565@gmail.com>
2022-08-12 11:55:04 +0300

    improvements

--- database_dao.py
+++ database_dao.py
@@ -1,13 +1,15 @@
 import sqlite3
 from constants import DATABASE
 
+from typing import List, Dict, AnyStr
+
 
 class DatabaseDAO:
 
     def __init__(self, sourse: str = DATABASE):
         self.sourse = sourse
 
-    def _get_data_from_db_by_query(self, query: str) -> list[dict]:
+    def _get_data_from_db_by_query(self, query: str) -> List[Dict]:
         """Create connection and get data from db by query"""
         with sqlite3.connect(self.sourse) as connection:
             cursor = connection.cursor()
@@ -17,7 +19,7 @@ class DatabaseDAO:
                 result.append(row)
             return result
 
-    def _get_formatted_result(self, *args, data_from_db) -> list[dict]:
+    def _get_formatted_result(self, *args, data_from_db) -> List[Dict]:
         """Revert data to needed format"""
         formatted_result = []
         for data in data_from_db:
@@ -27,7 +29,7 @@ class DatabaseDAO:
             formatted_result.append(dict_for_append)
         return formatted_result
 
-    def search_for_title(self, title) -> list[dict[str, str | int]]:
+    def search_for_title(self, title) -> List[Dict[AnyStr, AnyStr]]:
         """Create query and get data from db by title"""
         query = f"""
                     select title, country, release_year, listed_in, description
@@ -40,7 +42,7 @@ class DatabaseDAO:
                                             data_from_db=data_from_db)
         return result
 
-    def search_by_years_range(self, year_1, year_2) -> list[dict[str, str | int]]:
+    def search_by_years_range(self, year_1, year_2) -> List[Dict[AnyStr, AnyStr]]:
         query = f"""
                     select title, release_year
                     from netflix
@@ -62,7 +64,7 @@ class DatabaseDAO:
             return "rating like 'R' or rating like 'NC-17'"
         return None
 
-    def search_by_rating(self, rating_group: str) -> list[dict[str, str]]:
+    def search_by_rating(self, rating_group: str) -> List[Dict[AnyStr, AnyStr]]:
         rating_query = self._define_rating_query_part(rating_group)
         query = f"""
                     select title, rating, description
@@ -74,7 +76,7 @@ class DatabaseDAO:
         result = self._get_formatted_result('title', 'rating', 'description', data_from_db=data_from_db)
         return result
 
-    def search_by_genre(self, genre: str) -> list[dict[str, str]]:
+    def search_by_genre(self, genre: str) -> List[Dict[AnyStr, AnyStr]]:
         query = f"""
                     select title, description
                     from netflix
@@ -98,8 +100,6 @@ class DatabaseDAO:
         data_from_db = self._get_data_from_db_by_query(query)
         result = self._get_formatted_result('title', 'description', data_from_db=data_from_db)
         return result
-
-
 
 # db_dao = DatabaseDAO()
 # print(db_dao.search_by_type_release_year_genre('tv', 2010, 'comed'))
